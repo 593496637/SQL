5c7e9faf2b4f6994e2ae890e10fbdd40a5708728
云清淡雅 <593496637@qq.com>
2021-07-20 17:29:39 +0800

    sql

--- coderhub-用户表.sql
+++ coderhub-用户表.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `users`(
+CREATE TABLE IF NOT EXISTS `user`(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(50) NOT NULL,
@@ -9,3 +9,33 @@ CREATE TABLE IF NOT EXISTS `users`(
 INSERT INTO users (name,password) VALUES (?,?);
 
 SELECT	* FROM users WHERE name=?;
+
+
+CREATE TABLE IF NOT EXISTS `moment`(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	content VARCHAR(1000) NOT NULL,
+	user_id INT NOT NULL,
+	createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	FOREIGN KEY(user_id) REFERENCES user(id)
+);
+
+INSERT INTO moment (content,user_id) VALUES (?,?);
+
+SELECT * FROM moment WHERE id = ?;
+
+SELECT m.id id,m.content content,m.createAt createTime,m.updateAt updateTime,
+	JSON_OBJECT('id',u.id,'name',u.name) user
+	FROM moment m
+	LEFT JOIN user u ON m.user_id = u.id
+	WHERE m.id=1;
+	
+	
+SELECT m.id id,m.content content,m.createAt createTime,m.updateAt updateTime,
+	JSON_OBJECT('id',u.id,'name',u.name) user
+	FROM moment m
+	LEFT JOIN user u ON m.user_id = u.id
+	LIMIT ?,?;
+	
+	
+	
