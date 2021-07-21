4c9878d02ef277334db0d5eb4a6b875be23f1ebc
云清淡雅 <593496637@qq.com>
2021-07-21 17:29:06 +0800

    sql

--- coderhub-用户表.sql
+++ coderhub-用户表.sql
@@ -41,3 +41,27 @@ SELECT * FROM moment WHERE id=? AND user_id=?
 	
 UPDATE moment SET content = ? WHERE id = ?;
 	
+	
+CREATE TABLE IF NOT EXISTS comment(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	content VARCHAR(1000) NOT NULL,
+	moment_id INT NOT NULL,
+	user_id INT NOT NULL,
+	comment_id INT DEFAULT NULL,
+	createAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	FOREIGN KEY(moment_id) REFERENCES moment(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN key(comment_id) REFERENCES COMMENT(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+INSERT INTO comment(moment_id,content,user_id) VALUES (?,?,?);
+
+
+SELECT m.id id,m.content content,m.createAt createTime,m.updateAt updateTime,
+	JSON_OBJECT('id',u.id,'name',u.name) user,
+	(SELECT COUNT(*) FROM comment c WHERE c.moment_id = m.id) commentCount
+	FROM moment m
+	LEFT JOIN user u ON m.user_id = u.id
+	LIMIT 0,10;
