cb2fdc4c4c82f5325f21fe96e693984f38a03ccd
likk <593496637@qq.com>
2021-07-28 21:32:26 +0800

    lkk

--- coderhub-用户表.sql
+++ coderhub-用户表.sql
@@ -71,16 +71,29 @@ SELECT m.id id,m.content content,m.createAt createTime,m.updateAt updateTime,
 # 获取动态列表（包含评论/用户信息）
 SELECT 
 	m.id id,m.content content,m.createAt createTime,m.updateAt updateTime,
+<<<<<<< HEAD
 	JSON_OBJECT('id',u.id,'name',u.name,'avatarUrl',u.avatar_url) author,
 	JSON_ARRAYAGG(
 		JSON_OBJECT('id',c.id,'content',c.content,'commentId',c.comment_id,'createTime',c.createAt,
 							  'user',JSON_OBJECT('id',cu.id,'name',cu.name,'avatarUrl',u.avatar_url))
 		) comments
+=======
+	IF(COUNT(u.id),JSON_OBJECT('id',u.id,'name',u.name),JSON_ARRAY()) author,
+	IF(COUNT(l.id),JSON_ARRAYAGG(JSON_OBJECT('id',l.id,'name',l.name,'time',DATE_FORMAT(l.createAT,'%Y-%c-%d %H:%i:%s'))) ,JSON_ARRAY()) labels,
+	(SELECT IF(COUNT(c.id),JSON_ARRAYAGG(
+		JSON_OBJECT('id',c.id,'content',c.content,'commentId',c.comment_id,'createTime',DATE_FORMAT(c.createAt,'%Y-%c-%d %H:%i:%s'),
+							  'user',JSON_OBJECT('id',cu.id,'name',cu.name))
+		),JSON_ARRAY()) FROM comment c
+										LEFT JOIN user cu ON c.user_id = cu.id
+										WHERE m.id = c.moment_id) comments
+>>>>>>> 9466412 (lkk)
 FROM moment m
 LEFT JOIN user u ON m.user_id = u.id  
-LEFT JOIN comment c ON c.moment_id = m.id
-LEFT JOIN user cu ON c.user_id = cu.id
-WHERE m.id = 7
+
+LEFT JOIN moment_label ml ON m.id = ml.moment_id
+LEFT JOIN label l ON l.id = ml.label_id
+WHERE m.id = 4
+GROUP BY m.id;
 
 
 SELECT 
@@ -114,6 +127,7 @@ INSERT INTO label(name) VALUES (' C#');
 INSERT INTO label(name) VALUES (' python');
 INSERT INTO label(name) VALUES (' html');
 
+<<<<<<< HEAD
 
 SELECT * FROM label LIMIT 0,10;
 
@@ -146,4 +160,7 @@ ALTER TABLE `user` ADD avatar_url VARCHAR(200)
 
 
 
+=======
+SELECT * FROM label;
+>>>>>>> 9466412 (lkk)
 
